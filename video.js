329ed8fab0fbcf20cec01b14ddc2ffd451c631ae
MidorinoRyu <135086845+MidorinoRyu@users.noreply.github.com>
2023-08-01 13:09:14 -0600

    Update video.js

--- video.js
+++ video.js
@@ -7,30 +7,28 @@ const progreso = document.querySelector("#progreso")
 play.addEventListener("click", reproducir)
 pause.addEventListener("click", pausar)
 
-video.addEventListener("timeupdate", actualizarTiempo)  //esto es para que se meta el avance deel videeo a la barra
-video.addEventListener("loadedmetadata", tiempoLimite)  ///esto es para que el avance de el videeo utilice el 100% de la barra
-
+video.addEventListener("timeupdate", actualizarTiempo) 
+video.addEventListener("loadedmetadata", tiempoLimite) 
 function reproducir(){
-    console.log("diste click en boton de reproducir")
+    console.log("Diste click en Reproducir")
     video.play()
 }
 
 function pausar(){
-    console.log("diste click en boton de pausar")
+    console.log("Diste click en Pausar")
     video.pause()
 }
 
 function tiempoLimite(){
-    progreso.max = video.duration  //esto hace que la barra avance correspondientemente al video
+    progreso.max = video.duration  
 }
 
 function actualizarTiempo(){
-    ProgressEvent.value = video.currentTime  //genera el valor para utilizarse en el movimiento de la barra de tiempo
+    ProgressEvent.value = video.currentTime  
     
 }
 
-progreso.addEventListener("input", current)   //generamos el valor de la barra, para poder moverla
+progreso.addEventListener("input", current)  
 
 function current(){
-    video.currentTime = progreso.value    //con esto podemos mover la barrita y acomodar el video donde queramos
-}
+    video.currentTime = progreso.value    
